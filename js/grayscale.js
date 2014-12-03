a41684a60e518279f70782a1cc1afb77e2e1aba0
NathanBuckley <nathan@pinarylabs.com>
2014-12-03 08:08:08 +0000

    fixing map

--- js/grayscale.js
+++ js/grayscale.js
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(50.8429, 0.1313);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
