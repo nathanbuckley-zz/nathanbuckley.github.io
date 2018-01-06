7275a57c4bb4218c59c65b5a8f18190597f16673
nathanbuckley <nth.buckley@gmail.com>
2018-01-06 07:37:44 +0000

    more clean up

---
+++ uglifyAll.js
@@ -0,0 +1,4 @@
+/*
+* Will be run by shell scrip to minify all js and css befor deply.
+* Hate using and creating mess with tools like gulp. Just write it all myself with node and bash quickly becuase I can 
+*/
