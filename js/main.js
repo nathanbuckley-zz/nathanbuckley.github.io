9edad2a7fd16a6c7216a0a2236e12a0cd3092df0
nathanbuckley <nth.buckley@gmail.com>
2018-03-18 18:46:40 +0000

    working addition

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-//PArticles loader
+//Modal Workings
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".modalTrigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
