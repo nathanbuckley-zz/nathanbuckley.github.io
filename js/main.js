9c2886fe98b3f1e58c2631de2eee8a6e890f002d
nathanbuckley <nth.buckley@gmail.com>
2018-06-09 22:42:31 +0100

    Adding navigation, new areas and modal library

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
-//Modal Workings
+
+// Scrolling
+var gallery = document.getElementById("galNavbtn");
+var projects = document.getElementById("projNavbtn");
+var contact = document.getElementById("contNavbtn");
+
+gallery.onclick = function(){
+    var galdiv = document.getElementById("gal");
+    var galdivposition = galdiv.offsetTop;
+    window.scrollTo({
+        top: galdivposition,
+        behavior: "smooth"
+    });
+}
+
+projects.onclick = function(){
+    var projdiv = document.getElementById("proj");
+    var projdivposition = projdiv.offsetTop;
+    window.scrollTo({
+        top: projdivposition,
+        behavior: "smooth"
+    });
+}
+
+contact.onclick = function(){
+    var contdiv = document.getElementById("cont");
+    var contdivposition = contdiv.offsetTop;
+    window.scrollTo({
+        top: contdivposition,
+        behavior: "smooth"
+    });
+}
+
+
+
+//Temp Secret Modal
 
 var modal = document.querySelector(".modal");
 var trigger = document.querySelector(".modalTrigger");
 var closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
+function toggleModal(){
     modal.classList.toggle("show-modal");
 }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
+function windowOnClick(event){
+    if (event.target === modal){
         toggleModal();
     }
 }
