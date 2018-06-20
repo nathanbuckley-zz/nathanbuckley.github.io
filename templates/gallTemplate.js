fff51645f2016dc99e753e541243e8eba95fe81a
nathanbuckley <nth.buckley@gmail.com>
2018-06-20 14:26:26 +0100

    Gallery template complete

--- templates/gallTemplate.js
+++ templates/gallTemplate.js
@@ -5,20 +5,41 @@
 // instanciate new modal
 var galtempmodal = new tingle.modal({
     footer: true,
-    stickyFooter: false,
-    closeMethods: ['overlay','button', 'escape'],
+    stickyFooter: true,
+    closeMethods: ['overlay', 'escape'],
     closeLabel: "Close",
-    //cssClass: ['modal'],
+    cssClass: ['modal'],
 });
 
+
 // content
-galtempmodal.setContent('<h1>Gallery Template Modal</h1>');
+var content = '<h1>Gallery Template Modal</h1>' +
+'<img src="http://via.placeholder.com/500x300">' +
+'<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
+'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
+'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
+'<img src="http://via.placeholder.com/500x300">' +
+'<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
+'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
+'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
+'<img src="http://via.placeholder.com/500x300">';
+
+galtempmodal.setContent(content);
 
 // footer close button
-galtempmodal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+
+function randomBtnChoice(){
+    var footerBtnChoices = ['Finished','Done','Complete','Terminate'];
+    var rbc = footerBtnChoices[Math.floor(Math.random()*footerBtnChoices.length)];
+    return rbc;
+}
+
+galtempmodal.addFooterBtn(randomBtnChoice(), 'tingle-btn isobtn', function() {
     galtempmodal.close();
 });
 
+
+// Open Modal
 var galblockimgdom = document.getElementById('galblockimg');
 
 function opengaltempmodal() {
