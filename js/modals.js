d922030557f077f0ebb38efa9f2d3aa2bdb00afa
nathanbuckley <nth.buckley@gmail.com>
2018-06-22 23:08:15 +0100

    tweeks

--- js/modals.js
+++
@@ -1,8 +0,0 @@
-/*
-* Initinal File to hold first few test modals while
-* I get used to the library. Planned format is to have
-* a seperate file for each modal, with each one linked 
-* in the index.html file for use. This is for easy navigation
-* and formatting.
-*/
-
--- templates/gallTemplate.js
+++ templates/gallTemplate.js
@@ -14,15 +14,15 @@ var galtempmodal = new tingle.modal({
 
 // content
 var content = '<h1>Gallery Template Modal</h1>' +
-'<img src="http://via.placeholder.com/500x300">' +
+'<img src="http://via.placeholder.com/600x400">' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
-'<img src="http://via.placeholder.com/500x300">' +
+'<img src="http://via.placeholder.com/600x400">' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
-'<img src="http://via.placeholder.com/500x300">';
+'<img src="http://via.placeholder.com/600x400">';
 
 galtempmodal.setContent(content);
 
@@ -40,7 +40,7 @@ galtempmodal.addFooterBtn(randomBtnChoice(), 'tingle-btn isobtn', function() {
 
 
 // Open Modal
-var galblockimgdom = document.getElementById('galblockimg');
+var galblockimgdom = document.getElementById('galtempbtn');
 
 function opengaltempmodal() {
     galtempmodal.open();
