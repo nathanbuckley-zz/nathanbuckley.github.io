6276e31b4fc497d9ce7ee4e483713eee90b45106
nathanbuckley <nth.buckley@gmail.com>
2018-07-08 18:08:14 +0100

    Better Images right

--- js/gallmodals/cv3.js
+++ js/gallmodals/cv3.js
@@ -14,7 +14,7 @@ var mintcvmodal = new tingle.modal({
 
 // content
 var content = '<h1>Mint Charcoal CV Template (DOWNLOAD FREE)</h1>' +
-'<img src="img/modal/mintcv/mintcvimgmodal.png">' +
+'<img src="img/modal/mintcv/mintCvImgModal.png">' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
