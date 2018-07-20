c23ef835d7cbfc6c6ca0ef6825b76527d0d8dd03
nathanbuckley <nth.buckley@gmail.com>
2018-07-20 21:13:55 +0100

    temp

--- js/gallmodals/cv3.js
+++ js/gallmodals/cv3.js
@@ -18,7 +18,6 @@ var content = '<h1>Mint Charcoal CV Template (DOWNLOAD FREE)</h1>' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
-'<img src="http://via.placeholder.com/600x400">' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>' +
