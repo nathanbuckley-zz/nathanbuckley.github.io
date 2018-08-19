d8b37723eb5fc7ea69796aa1f64b99da6713d7ef
nathanbuckley <nth.buckley@gmail.com>
2018-08-19 10:32:42 +0100

    additional project

--- js/essaymodals/thebeginning.js
+++ js/essaymodals/thebeginning.js
@@ -14,7 +14,7 @@ var beginningmodal = new tingle.modal({
 
 // content
 var content = '<h1>A Beginning that feels like the end...</h1>' +
-'<img src="http://via.placeholder.com/600x400">' +
+'<img src="http://via.placeholder.com/600x300">' +
 '<p>Wafer soufflé jelly-o candy canes gummi bears. Lollipop candy dessert pastry. Sugar plum chocolate bar cupcake dessert' +
 'halvah marshmallow marshmallow brownie powder. Icing jelly beans sesame snaps cake. Gummies gingerbread cotton candy' +
 'chocolate cake jelly cake candy. Jelly beans cookie brownie jelly beans donut lollipop.</p>';
